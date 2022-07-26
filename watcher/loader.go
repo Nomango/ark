788c10e13f43a6e3fc2f7f360532ec9b54bfb4b8
haibo2.liu <haibo2.liu@ly.com>
2022-07-26 10:39:46 +0800

    add loader options

--- watcher/loader.go
+++ watcher/loader.go
@@ -6,21 +6,36 @@ import (
 )
 
 // AutoLoad updates returned atomic value every time n triggers
-func AutoLoad(ctx context.Context, n Notifier) *atomic.Value {
-	l := NewLoader(n)
+func AutoLoad(ctx context.Context, n Notifier, opts ...LoaderOption) *atomic.Value {
+	l := NewLoader(n, opts...)
 	l.Start(ctx)
 	return &l.v
 }
 
+type Transformer = func(context.Context, interface{}) interface{}
+
 type Loader struct {
 	w *Watcher
 	v atomic.Value
+
+	transforms []Transformer
 }
 
-func NewLoader(n Notifier) *Loader {
+func NewLoader(n Notifier, opts ...LoaderOption) *Loader {
 	l := &Loader{}
 	l.w = NewWatcher(n, l.receive)
+	for _, opt := range opts {
+		opt(l)
+	}
 	return l
+}
+
+type LoaderOption func(*Loader)
+
+func WithTransformer(t Transformer) LoaderOption {
+	return func(l *Loader) {
+		l.transforms = append(l.transforms, t)
+	}
 }
 
 func (l *Loader) Get() interface{} {
@@ -36,5 +51,8 @@ func (l *Loader) Stop() {
 }
 
 func (l *Loader) receive(ctx context.Context, v interface{}) {
+	for _, t := range l.transforms {
+		v = t(ctx, v)
+	}
 	l.v.Store(v)
 }
