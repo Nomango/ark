578ceb81ac0b94fbc12f25c55683c999ff8ecc41
Haibo <nomango@qq.com>
2023-01-31 10:28:46 +0800

    update env

--- envs/env.go
+++ envs/env.go
@@ -1,4 +1,4 @@
-package envs
+package env
 
 import (
 	"flag"
@@ -6,45 +6,73 @@ import (
 	"sync"
 )
 
-func IsProd() bool {
-	return Is(envFlagProd)
+// Flag environment flag
+type Flag string
+
+const (
+	FlagProduct Flag = "product"
+	FlagStaging Flag = "staging"
+	FlagDev     Flag = "dev"
+	FlagTesting Flag = "testing"
+)
+
+// DefaultEnvKey is the default environment variable
+const DefaultEnvKey = "ENV"
+
+var flagInitFunc = InitFromEnv(DefaultEnvKey)
+
+// InitFromEnv 从环境变量读取环境配置
+func InitFromEnv(envKey string) func() (Flag, bool) {
+	return func() (Flag, bool) {
+		flag, ok := os.LookupEnv(envKey)
+		return Flag(flag), ok
+	}
 }
 
-func IsDev() bool {
-	return Is(envFlagDev)
+// SetInitFunc 设置环境初始化函数
+func SetInitFunc(f func() (Flag, bool)) {
+	flagInitFunc = f
 }
 
-func IsTesting() bool {
-	return Is(envFlagTesting)
-}
-
-func Is(flag string) bool {
-	return getFlag() == flag
-}
-
-func Name() string {
+// Current 返回当前环境名
+func Current() Flag {
 	return getFlag()
 }
 
+// IsTesting 判断是否是测试环境
+func IsTesting() bool {
+	return Current() == FlagTesting
+}
+
+// IsDev 判断是否是本地开发环境
+func IsDev() bool {
+	return Current() == FlagDev
+}
+
+// IsStaging 判断是否是预览环境
+func IsStaging() bool {
+	return Current() == FlagStaging
+}
+
+// IsProduct 判断是否是线上环境
+func IsProduct() bool {
+	return Current() == FlagProduct
+}
+
 var (
+	envFlag Flag
 	once    sync.Once
-	envFlag string
 )
 
-const (
-	envFlagProd    = "prod"
-	envFlagDev     = "dev"
-	envFlagTesting = "testing"
-)
-
-func getFlag() string {
+func getFlag() Flag {
 	once.Do(func() {
 		if flag.Lookup("test.v") != nil {
-			envFlag = envFlagTesting
-		} else if f, ok := os.LookupEnv("ENV"); ok {
+			envFlag = FlagTesting
+		} else if f, ok := flagInitFunc(); ok {
 			envFlag = f
-		} else {
-			envFlag = envFlagProd
+		}
+		if envFlag == "" {
+			panic("init environment flag failed!")
 		}
 	})
 	return envFlag
